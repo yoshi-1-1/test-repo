7362679407044bdf511d059539246f7f54bee874
YOSHI <yoshi.1.ym@gmail.com>
2026-07-12 10:22:10 +0900

    csvの読み込み

--- mkf.c
+++ mkf.c
@@ -2,9 +2,23 @@
 
 int	main(void)
 {
+	int		i;
+	int		j;
 	FILE	*file;
 
-	file = fopen ("text.txt", "w");
+	file = fopen("text.txt", "r");
+	if (file == NULL)
+	{
+		printf("error\n");
+		return (1);
+	}
+	if (fscanf(file, "%d,%d", &i, &j) != 2)
+	{
+		printf("error\n");
+		fclose(file);
+		return (1);
+	}
+	printf ("%d:%d", i, j);
 	fclose (file);
 	return (0);
 }
