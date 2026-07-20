4c4f264f6809f246ceb7f1ccb463eb8b7470643b
YOSHI <yoshi.1.ym@gmail.com>
2026-07-20 09:35:57 +0900

    再帰処理の練習

---
+++ ft_putnbr.c
@@ -0,0 +1,26 @@
+#include <unistd.h>
+#include "ft_putnbr.h"
+
+void	ft_putnbr(int nb)
+{
+	char	c;
+
+	if (nb >= 0 && nb <= 9)
+	{
+		c = nb + '0';
+		write (1, &c, 1);
+	}
+	if (nb >= 10)
+	{
+		ft_putnbr(nb / 10);
+		ft_putnbr(nb % 10);
+	}
+	return ;
+}
+
+int	main(void)
+{
+	ft_putnbr(123);
+	write (1, "\n", 1);
+	return (0);
+}
---
+++ ft_putnbr.h
@@ -0,0 +1,8 @@
+#ifndef FT_PUTNBR_H
+# define FT_PUTNBR_H
+
+# include <unistd.h>
+
+void		ft_putnbr(int nb);
+
+#endif
